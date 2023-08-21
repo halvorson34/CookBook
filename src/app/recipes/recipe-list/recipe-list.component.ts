120e425e36f82524c8190518218ebceb8ecd8c69
[Matt Halvorson] <[matt.halvorson@hrblock.com]>
2023-08-21 13:43:56 -0500

    add content to recipe list componenet

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe("Title of Recipe", "Description of Recipe", "https://th.bing.com/th/id/OIP.41AA68Z2szUwM4fg-IqzTwHaE7?w=268&h=180&c=7&r=0&o=5&pid=1.7")
+  ];
 
   constructor() { }
 
