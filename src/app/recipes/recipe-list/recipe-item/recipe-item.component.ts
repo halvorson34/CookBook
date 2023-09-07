eb27106b03552fe91dd8553c31ebaf797a6e07b2
[Matt Halvorson] <[matt.halvorson@hrblock.com]>
2023-09-07 13:01:50 -0500

    moved recipe item from list to item component

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Recipe } from './../../recipe.model';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
-export class RecipeItemComponent {
+export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe;
 
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
 }
